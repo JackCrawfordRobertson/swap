363cbe74a0944c64ac100fb360f662989cdd9a9e
Jack Robetson <jackrobertsonps3@gmail.com>
2024-07-23 18:02:56 +0100

    Uodates to confi

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+
 const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: 'AIzaSyD527_pJzixokcoxfR4AeiMIBTXfhfvpis',
   },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  }
 };
 
 export default nextConfig;
