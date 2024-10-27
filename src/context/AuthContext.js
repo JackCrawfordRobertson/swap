4b767e617f21341507fecf1710fa96833b5b89ff
Jack Robetson <jackrobertsonps3@gmail.com>
2024-10-27 11:14:51 +0000

    auth update

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,6 @@
+'use client';
+// src/context/AuthContext.js
+
 import React, { createContext, useState, useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../config/firebaseConfig';
@@ -6,22 +9,30 @@ export const AuthContext = createContext({ user: null });
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true); // Added loading state
 
   useEffect(() => {
-    console.log("Listening for auth state changes...");
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      console.log("Auth state changed:", currentUser);
-      setUser(currentUser || null);
-      setLoading(false);
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        setUser(null);
+      }
+      setLoading(false); // Set loading to false when auth state is known
     });
-    return () => unsubscribe();
+
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
-  if (loading) return null;
+  if (loading) {
+    // You can return a loading indicator here if you prefer
+    return null;
+  }
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, loading }}>
       {children}
     </AuthContext.Provider>
   );
